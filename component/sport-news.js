8ffbf265557a57c0e124fe96ded190fa2ec585da
surenjamts <surenjamts@gmail.com>
2023-05-15 12:57:48 +0800

    css zasav, component zasav

--- component/sport-news.js
+++ component/sport-news.js
@@ -1,35 +1,31 @@
-
 import html from "./utility.js";
+
+const template = document.getElementById('temp').content;
 
 class Sportnews extends HTMLElement {
     constructor() {
         super(); // always call super() first in the ctor.
         this.count = 0;
-        this.innerHTML = html`
-        <div class="sport-news">
-        
-            <img src="https://picsum.photos/300/150.webp?random=${Math.random() * 100}" alt="">
-            <p>asdf arwr sdfas dfawer asdfasdfaewr</p>
-         
-            <button role="button"><i id="icon" class="fa-solid fa-bookmark fa-2x"></i></button>
-            
-        </div>`
-        
+        this.attachShadow({mode:"open"});
+        this.shadowRoot.appendChild(template.cloneNode(true));
+        //this.querySelector("p").innerText = this.getAttribute('garchig');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('zurag');
     }
     connectedCallback() {
-        this.querySelector("button").addEventListener("click", () => {
+        this.shadowRoot.querySelector("button").addEventListener("click", () => {
+            console.log("gg");
             const myCart = document.querySelector("news-count");
-            myCart.AddToCart(this);
+            myCart.AddToCart(this.shadowRoot);
             myCart.color = "#0f0";
-            this.count=this.count+1;
-            const icon = this.querySelector("#icon");
-            if (this.count %2==0) {
+            this.shadowRoot.count=this.shadowRoot.count+1;
+            const icon = this.shadowRoot.querySelector("#icon");
+            if (this.shadowRoot.count %2==0) {
             icon.setAttribute("style", "color: black");
             }
             else
             icon.setAttribute("style", "color: red");
             const news = document.querySelector("news-save");
-            news.AddToCart(this);
+            news.AddToCart(this.shadowRoot);
             news.color = "#0f0";
             
         })
--- component/sport-newslist.js
+++ component/sport-newslist.js
@@ -4,25 +4,6 @@ import "./sport-news.js";
 class SportnewsList extends HTMLElement {
     constructor() {
         super(); // always call super() first in the ctor.
-        this.innerHTML = `
-        <style> 
-        .sport-newslist{
-            display:flex;
-            flex-direction: ${ this.getAttribute("direction") }; 
-            gap:2ch;
-            flex-wrap: wrap;}
-
-        .sport-newslist > sport-news{
-            flex: 1 1;
-        }
-      </style>
-    <div class="sport-newslist">
-        <sport-news></sport-news>
-        <sport-news></sport-news>
-        <sport-news></sport-news>
-    
-    
-    </div>`;
     }
     connectedCallback() {
 
