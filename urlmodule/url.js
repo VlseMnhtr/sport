6cd5b7ab5cbbdcb936b6f18a653794876397e760
Zayatszz <zayaz3802@gmail.com>
2023-05-15 17:06:27 +0800

    design

--- urlmodule/url.js
+++
@@ -1,8 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-
-const sportItem = recentNews._recentNewsList.find(item => item.id === id);
-const name = sportItem ? sportItem.name : "Unknown";
-
-const sportName = document.getElementById("sport-name");
-sportName.textContent = name;
