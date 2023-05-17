7ebdee2622a3ab77d42712ef37fcf43b7070bfc0
Zayatszz <zayaz3802@gmail.com>
2023-05-18 00:22:28 +0800

    index-mobile<768px

--- component/news-count.js
+++ component/news-count.js
@@ -13,7 +13,7 @@ class NewsSave extends HTMLElement {
 
     #Render(backgroundColor = "#fff") {
         this.innerHTML = html`
-        <div style="width:2rem;height:auto;background-color:${backgroundColor};"><svg version="1.1"
+        <div  style="width:2rem;height:auto;background-color:${backgroundColor};"><svg version="1.1"
         xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
         <metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata>
